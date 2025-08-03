fe8ef99f1f38162662b1200353a3288ebfab48fd
Rozila28 <putrarozila@gmail.com>
2025-08-03 21:26:28 +0800

    switch-case

--- conditional.js
+++ conditional.js
@@ -34,3 +34,17 @@ switch (hari) {
   default:
     console.log("Hari Santay");
 }
+let text;
+switch (new Date().getDay()) {
+  case 4:
+  case 5:
+    console.log("Soon it is Weekend");
+    break;
+  case 0:
+  case 6:
+     console.log("It is Weekend");
+    break;
+  default:
+    console.log("Looking forward to the Weekend");
+}
+console.log(new Date().getDay())
